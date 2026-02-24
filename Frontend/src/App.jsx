8dedaa52553b1a44e8f3eeebe3a90a4ca9fcef7a
yubaaa <chaib9092@gmail.com>
2026-02-24 23:01:06 +0100

    second one is great

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -41,7 +41,7 @@ function App() {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
-        },
+        }, 
         body: JSON.stringify(formData)
       });
 
