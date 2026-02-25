9c9045bbe6c219e22aaa8ea987c8980cd0c63a79
yubaaa <chaib9092@gmail.com>
2026-02-25 07:50:00 +0100

    Here's the new back

--- Backend/server.js
+++ Backend/server.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 app.post("/api/reset-password", (req, res) => {
-  const { email, currentPassword, newPassword } = req.body;
+  const { email, currentPassword, newPassword, confirmPassword } = req.body;
 
   // Vérification basique
   if (!email || !currentPassword || !newPassword) {
@@ -16,8 +16,11 @@ app.post("/api/reset-password", (req, res) => {
     });
   }
 
-  console.log("BODY COMPLET REÇU:");
-  console.log(req.body);
+
+  console.log("---- RESET DEMANDE ----");
+  console.log("Email:", email);
+  console.log("Current:", currentPassword);
+  console.log("New:", newPassword);
 
   return res.status(200).json({
     message: "Mot de passe mis à jour avec succès ."
